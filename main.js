bccd7bbacc61666f160dd14c6f45774efd39ab41
Natashayp <96372804+Natashayp@users.noreply.github.com>
2023-10-22 19:09:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -20,23 +20,5 @@ function validateForm() {
     return false;
   }
   
-  if (nrp.value.substring(0,4) != 5026)
-  {
-    alert("Hanya untuk mahasiswa SI" );
-    nrp.focus();
-
-    return false; //function ini ada return value, bukan void 
-  }
-
-  if(isNaN(date.value.substring(0,2)) == true && isNaN(date.value.substring(3,5)) == true && isNaN(date.value.substring(6)) == true)
-  {
-    alert("Enter date in format dd-mm-yy ")
-    date.focus();
-  }
-  return true;
-
 }
-
-
-
 
